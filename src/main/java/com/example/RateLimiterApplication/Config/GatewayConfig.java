0c6fb70c333bea53eebe3a26c41df23cc465aa6c
VISHALRG03 <vishalgon45@gmail.com>
2026-02-05 20:00:08 +0530

    Initial commit - Redis Rate Limiter

---
+++ src/main/java/com/example/RateLimiterApplication/Config/GatewayConfig.java
@@ -0,0 +1,40 @@
+package com.example.RateLimiterApplication.Config;
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Configuration;
+import com.example.RateLimiterApplication.Filter.TokenBucketRateLimiterFilter;
+import com.example.RateLimiterApplication.Service.RateLimiterService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+//defined how spring cloud gateway routes the incoming requests.
+// client -> backend services
+
+@Configuration
+public class GatewayConfig {
+
+    private final RateLimiterProperties rateLimiterProperties;
+    private final TokenBucketRateLimiterFilter tokenBucketRateLimiterFilter;
+
+
+    public GatewayConfig(RateLimiterProperties rateLimiterProperties, TokenBucketRateLimiterFilter tokenBucketRateLimiterFilter){
+        this.rateLimiterProperties = rateLimiterProperties;
+        this.tokenBucketRateLimiterFilter = tokenBucketRateLimiterFilter;
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route("api-route", r -> r
+                        .path("/api/**")
+                        .filters(f -> f
+                                .stripPrefix(1)
+                                .filter(tokenBucketRateLimiterFilter.apply(new TokenBucketRateLimiterFilter.Config()))
+                        )
+                        .uri(rateLimiterProperties.getApiServerUrl()))
+                .build();
+    }
+
+}
+
+// Request arrives > stripPrefix(1) > remove /api/prefix > TokenBucketRateLimiterFilter > api-server-url
---
+++ src/main/java/com/example/RateLimiterApplication/Config/RateLimiterProperties.java
@@ -0,0 +1,20 @@
+package com.example.RateLimiterApplication.Config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "rate-limiter")
+public class RateLimiterProperties {
+
+    private long capacity = 10;
+
+    private long refillRate = 1;
+
+
+    private String apiServerUrl = "http://localhost:8080";
+
+    private int timeout = 5000;
+}
